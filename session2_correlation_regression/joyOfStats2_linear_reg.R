0506b0fa95683c50ea9b4217c67042b8131e13a0
Rodrigo O. Polo <ropolomx@users.noreply.github.com>
2018-03-27 19:01:18 -0600

    Update joyOfStats2_linear_reg.R

--- session2_correlation_regression/joyOfStats2_linear_reg.R
+++ session2_correlation_regression/joyOfStats2_linear_reg.R
@@ -14,8 +14,24 @@ Fit  <- lm (mydata$Sepal.Length ~ mydata$Petal.Length)
 summary(Fit)
 
 
-plot(mydata$Sepal.Length ~ mydata$Petal.Length,col = "blue",main = "mydata$Sepal.Length &  mydata$Petal.Length Regression",cex = 1.3,pch = 16,xlab = "Sepal.Length",ylab = "Sepal.Width",xlim = c(3,8),ylim=c(3,8))
+plot(mydata$Sepal.Length ~ mydata$Petal.Length,
+     col = "blue",
+     main = "mydata$Sepal.Length &  mydata$Petal.Length Regression",
+     cex = 1.3,
+     pch = 16,
+     xlab = "Sepal.Length",
+     ylab = "Sepal.Width",
+     xlim = c(3,8),
+     ylim=c(3,8)
+    )
 
 abline(lm(mydata$Sepal.Length ~ mydata$Petal.Length), col="red", lwd=3, lty=1)
 
-text(6, 5, paste("Y =", round(Fit$coefficients[1],3), "+" , round(Fit$coefficients[2],3),"* X", "\nR-squared:", summary(Fit)$adj.r.squared), cex = 1.5,col="red")
+text(6, 5, paste("Y =", 
+                 round(Fit$coefficients[1],3), 
+                 "+" , 
+                 round(Fit$coefficients[2],3),
+                 "* X", "\nR-squared:", 
+                 summary(Fit)$adj.r.squared), 
+     cex = 1.5,
+     col="red")
